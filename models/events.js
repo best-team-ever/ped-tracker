f048e568b796c5f370d9ee45033167c24c7c2a71
Jean-Philippe Bornier <jeanphilippe.bornier@decathlon.com>
2018-06-19 17:05:41 +0200

    database init

---
+++ models/events.js
@@ -0,0 +1,13 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  var events = sequelize.define('events', {
+    user_id: DataTypes.INTEGER,
+    device_id: DataTypes.INTEGER,
+    user_id: DataTypes.INTEGER,
+    message: DataTypes.STRING
+  }, {});
+  events.associate = function(models) {
+    // associations can be defined here
+  };
+  return events;
+};
