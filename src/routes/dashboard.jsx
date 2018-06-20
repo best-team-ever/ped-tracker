cc47bd7b9222b765439622b9ca6bbc56c09436db
Jean-Philippe Bornier <jeanphilippe.bornier@decathlon.com>
2018-06-20 13:54:44 +0200

    add designn first version

---
+++ src/routes/dashboard.jsx
@@ -0,0 +1,74 @@
+import Dashboard from "views/Dashboard/Dashboard";
+import Devices from "views/Devices/Devices";
+import Locations from "views/Locations/Locations";
+import Users from "views/Users/Users";
+import Help from "views/Help/Help";
+
+import UserProfile from "views/UserProfile/UserProfile";
+import Typography from "views/Typography/Typography";
+import Icons from "views/Icons/Icons";
+import Notifications from "views/Notifications/Notifications";
+
+const dashboardRoutes = [
+  {
+    path: "/dashboard",
+    name: "Dashboard",
+    icon: "pe-7s-graph",
+    component: Dashboard
+  },
+  {
+    path: "/devices",
+    name: "Devices",
+    icon: "pe-7s-calculator",
+    component: Devices
+  },
+  {
+    path: "/locations",
+    name: "Locations",
+    icon: "pe-7s-map-2",
+    component: Locations
+  },
+  {
+    path: "/users",
+    name: "Users",
+    icon: "pe-7s-users",
+    component: Users
+  },
+  {
+    path: "/help",
+    name: "Help",
+    icon: "pe-7s-help1",
+    component: Help
+  },
+
+  {
+    path: "/form",
+    name: "Sample form",
+    icon: "pe-7s-user",
+    component: UserProfile
+  },
+  {
+    path: "/typography",
+    name: "Catalog Typography",
+    icon: "pe-7s-news-paper",
+    component: Typography
+  },
+  {
+    path: "/icons",
+    name: "Catalog Icons",
+    icon: "pe-7s-science",
+    component: Icons },
+  {
+    path: "/notifications",
+    name: "Catalog Notifications",
+    icon: "pe-7s-bell",
+    component: Notifications
+  },
+  {
+    redirect: true,
+    path: "/",
+    to: "/dashboard",
+    name: "Dashboard" }
+];
+
+export default dashboardRoutes;
---
+++ src/views/Devices/Devices.jsx
@@ -0,0 +1,59 @@
+import React, { Component } from "react";
+import { Grid, Row, Col, Table } from "react-bootstrap";
+import ReactTable from "react-table";
+import 'react-table/react-table.css'
+
+import Card from "components/Card/Card.jsx";
+
+const rthArray = [
+  { Header: 'ID', accessor: 'id', maxWidth: 100},
+  { Header: 'Name', accessor: 'name',
+     filterMethod: (filter, row) => row[filter.id].startsWith(filter.value)
+  },
+  { Header: 'Salary', accessor: 'salary'},
+  { Header: 'Country', accessor: 'country'},
+  { Header: 'City', accessor: 'city'},
+];
+const rtdArray = [
+  {id: "1", name: "Dakota Rice", salary: "$36,738", country: "Niger", city: "Oud-Turnhout"},
+  {id: "2", name: "Minerva Hooper", salary: "$23,789", country: "Curaçao", city: "Sinaai-Waas"},
+  {id: "3", name: "Sage Rodriguez", salary: "$56,142", country: "Netherlands", city: "Baileux"},
+  {id: "4", name: "Philip Chaney", salary: "$38,735", country: "Korea, South", city: "Overland Park"},
+  {id: "5", name: "Doris Greene", salary: "$63,542", country: "Malawi", city: "Feldkirchen in Kärnten"},
+  {id: "6", name: "Mason Porter", salary: "$78,615", country: "Chile", city: "Gloucester"},
+];
+
+class Devices extends Component {
+  render() {
+    return (
+      <div className="content">
+        <Grid fluid>
+          <Row>
+            <Col md={12}>
+              <Card
+                title="Devices"
+                category="with react-table component"
+                ctTableFullWidth
+                ctTableResponsive
+                content={
+                  <ReactTable
+                    noDataText="Not devices available!"
+                    columns={rthArray}
+                    data={rtdArray}
+                    defaultPageSize={10}
+                    className="-striped -highlight"
+                    defaultSorted={[{ id: "name", desc: true}]}
+                    filterable
+                    defaultFilterMethod={(filter, row) => String(row[filter.id]) === filter.value}
+                  />
+                }
+              />
+            </Col>
+          </Row>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+export default Devices;
