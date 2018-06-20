5ed0eb85486c4ce4962d6822a90f276f836197b2
Yuanqin DENG <yuanqin.deng@decathlon.com>
2018-06-20 17:40:59 +0200

    api for location

--- models/events.js
+++ models/events.js
@@ -1,12 +1,44 @@
 'use strict';
-module.exports = (sequelize, DataTypes) => {
-  var events = sequelize.define('events', {
-    user_id: DataTypes.INTEGER,
-    device_id: DataTypes.INTEGER,
-    message: DataTypes.STRING
-  }, {});
-  events.associate = function(models) {
-    // associations can be defined here
-  };
-  return events;
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.sequelize
+      .query('CREATE EXTENSION IF NOT EXISTS "pgcrypto";')
+      .then(() =>
+        queryInterface.createTable('events', {
+          id: {
+            allowNull: false,
+            autoIncrement: false,
+            primaryKey: true,
+            type: Sequelize.UUID,
+            defaultValue: Sequelize.literal("gen_random_uuid()")
+          },
+          user_id: {
+            type: Sequelize.UUID,
+            references: { model: 'users', key: 'id' }
+          },
+          device_id: {
+            type: Sequelize.UUID,
+            references: { model: 'devices', key: 'id' }
+          },
+          message: {
+            type: Sequelize.STRING
+          },
+          location_id: {
+            type: Sequelize.UUID,
+            references: { model: 'locations', key: 'id' }
+          },
+          createdAt: {
+            allowNull: false,
+            type: Sequelize.DATE
+          },
+          updatedAt: {
+            allowNull: false,
+            type: Sequelize.DATE
+          }
+        })
+      );
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('events');
+  }
 };
--- seeders/20180619171613-demo-locations.js
+++ seeders/20180619171613-demo-locations.js
@@ -2,19 +2,50 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) => {
-    return queryInterface.bulkInsert('locations', [{
-      location_type: "store",
-      name: "GILTBROOK",
-      address: "Ikea Retail Park, Nottingham NG16 2RP",
-      country: "UK",
-      contact_name: "Connell Nick",
-      contact_position: "Operations Manager",
-      contact_phone: "44115 938 2020",
-      contact_email: "nick.connell@decathlon.com",
-      status: "active",
-      createdAt: new Date(),
-      updatedAt: new Date()
-    }], {});
+    return queryInterface.bulkInsert('locations', [
+      {id: "16213c8f-ba02-4120-a1cc-0e735a12d81a",
+        location_type: "store",
+        name: "GILTBROOK",
+        site_id: 260,
+        address: "Ikea Retail Park, Nottingham NG16 2RP",
+        country: "UK",
+        contact_name: "Connell Nick",
+        contact_position: "Operations Manager",
+        contact_phone: "44115 938 2020",
+        contact_email: "nick.connell@decathlon.com",
+        status: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {id: "1d72faa0-318a-44c1-a15a-87f583094d7f",
+        location_type: "store",
+        name: "SURREY QUAYS",
+        site_id: 240,
+        address: "Canada Water Retail Park, Surrey Quays Road, London SE16 2XU",
+        country: "UK",
+        contact_name: "Gutierrez Antonio",
+        contact_position: "Operations Manager",
+        contact_phone: "4420 7394 2000",
+        contact_email: "antonio.gutierrez1@decathlon.com",
+        status: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      },
+      {id: "f14b2e9e-e06d-4cbe-9f32-4819a4a8e699",
+        location_type: "store",
+        name: "SHEFFIELD",
+        site_id: 240,
+        address: " 199 Eyre St, Sheffield, South Yorkshire S1 3HU",
+        country: "UK",
+        contact_name: "Goddart Liz",
+        contact_position: "Operations Manager",
+        contact_phone: "44114 229 8190",
+        contact_email: "liz.goddard@decathlon.com",
+        status: 1,
+        createdAt: new Date(),
+        updatedAt: new Date()
+      }
+    ], {});
   },
 
   down: (queryInterface, Sequelize) => {
