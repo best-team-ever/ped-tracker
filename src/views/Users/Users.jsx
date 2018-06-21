61988858fd853bb881edcb99ad4d20eaf80794f0
OLIVIER MASUREL <olivier.masurel@decathlon.COM>
2018-06-21 15:37:56 +0200

    User screen 1 MAJ

--- src/views/Users/Users.jsx
+++ src/views/Users/Users.jsx
@@ -1,50 +1,111 @@
 import React, { Component } from "react";
-import { Grid, Row, Col, Table } from "react-bootstrap";
+import ReactTable from "react-table";
+import { Link } from "react-router-dom";
+import 'react-table/react-table.css'
 
-import Card from "components/Card/Card.jsx";
-import { thArray, tdArray } from "variables/Variables.jsx";
+import TableCard from "components/TableCard/TableCard.jsx";
+
+
+const rtdArray = [
+    {id: "40dc09ad-c035-47cf-8587-8622c4319d86",
+    first_name: "jeanphilippe",
+    last_name: "bornier",
+    email: "jeanphilippe.bornier@decathlon.com",
+    p2pe_agreement: 1,
+    language: "en",
+    role: "hotesse",
+    location: "GILTBROOK",
+    createdAt: new Date(),
+    updatedAt: new Date()
+    },
+    {id: "700cb59b-91fd-42b3-aa41-3702f0b8611d",
+    first_name:"yuanqin",
+    last_name: "deng",
+    email: "yuanqin.deng@decathlon.com",
+    p2pe_agreement: 1,
+    language: "en",
+    role: "admin",
+    location: "GILTBROOK",
+    createdAt: new Date(),
+    updatedAt: new Date()
+    },
+    {id:"ae6233bb-90db-42bc-8f18-17a0d31c0100",
+    first_name:"olivier",
+    last_name: "masurel",
+    email: "olivier.masurel@decathlon.com",
+    p2pe_agreement: 0,
+    language: "en",
+    role: "hotesse",
+    location_id: "GILTBROOK",
+    createdAt: new Date(),
+    updatedAt: new Date()
+    },
+    {id:"66defb1c-241d-4337-a835-3d388c78e8bf",
+    first_name:"frederic",
+    last_name: "delimeux",
+    email: "frederic.delimeux@decathlon.com",
+    p2pe_agreement: 0,
+    language: "en",
+    role: "admin",
+    location: "GILTBROOK",
+    createdAt: new Date(),
+    updatedAt: new Date()
+    }
+];
+
 
 class Users extends Component {
-  render() {
-    return (
-      <div className="content">
-        <Grid fluid>
-          <Row>
-            <Col md={12}>
-              <Card
-                title="Striped Table with Hover"
-                category="Here is a subtitle for this table"
-                ctTableFullWidth
-                ctTableResponsive
-                content={
-                  <Table striped hover>
-                    <thead>
-                      <tr>
-                        {thArray.map((prop, key) => {
-                          return <th key={key}>{prop}</th>;
-                        })}
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {tdArray.map((prop, key) => {
-                        return (
-                          <tr key={key}>
-                            {prop.map((prop, key) => {
-                              return <td key={key}>{prop}</td>;
-                            })}
-                          </tr>
-                        );
-                      })}
-                    </tbody>
-                  </Table>
-                }
-              />
-            </Col>
-          </Row>
-        </Grid>
-      </div>
-    );
+  onClick = (event) => {
+    console.log(event);
   }
-}
+
+  // const thArray = ["First name", "Last name", "Email", "P2PE Agreement", "Role", "Location"];
+  render() {
+    const rthArray = [
+      { Header: 'ID', accessor: 'id', maxWidth: 100},
+      { Header: 'First name', accessor: 'first_name'},
+      { Header: 'Last name', accessor: 'last_name'},
+      { Header: 'Email', accessor: 'email'},
+      { Header: 'P2PE Agreement', accessor: 'p2pe_agreement'},
+      { Header: 'Role', accessor: 'role'},
+      { Header: 'Location', accessor: 'location_id'},
+      { Header: 'Action',
+        Cell: (row)=>(
+          <div className="action-right">
+            <Link to={`/devices/${row.row.id}`} className="btn-simple btn-icon btn btn-warning">
+              <i className="fa fa-edit"></i>
+            </Link>
+          </div>
+        ),
+        filterable: false, maxWidth: 100
+      },
+    ];
+
+      return (
+        <div className="content">
+          <TableCard
+            title="Devices"
+            category="list of devices"
+            ctTableFullWidth
+            ctTableResponsive
+            addButton="New device"
+            content={
+              <ReactTable
+                noDataText="Empty list!"
+                columns={rthArray}
+                data={rtdArray}
+                defaultPageSize={10}
+                className="-striped -highlight"
+                defaultSorted={[{ id: "name", desc: true}]}
+                filterable
+                defaultFilterMethod={(filter, row) => row[filter.id].startsWith(filter.value)}
+              />
+            }
+          />
+        </div>
+      );
+    }
+  }
+
 
 export default Users;
