8de0dbdb411af40b965e81bca0fa6eaff50b846f
Jean-Philippe Bornier <jeanphilippe.bornier@decathlon.com>
2018-06-21 15:30:02 +0200

    device form

--- src/views/Devices/Devices.jsx
+++ src/views/Devices/Devices.jsx
@@ -6,15 +6,6 @@ import { Link } from "react-router-dom";
 import 'react-table/react-table.css'
 import TableCard from "components/TableCard/TableCard.jsx";
 
-// const rtdArray = [
-//   {id: "1", name: "Dakota Rice", salary: "$36,738", country: "Niger", city: "Oud-Turnhout"},
-//   {id: "2", name: "Minerva Hooper", salary: "$23,789", country: "Curaçao", city: "Sinaai-Waas"},
-//   {id: "3", name: "Sage Rodriguez", salary: "$56,142", country: "Netherlands", city: "Baileux"},
-//   {id: "4", name: "Philip Chaney", salary: "$38,735", country: "Korea, South", city: "Overland Park"},
-//   {id: "5", name: "Doris Greene", salary: "$63,542", country: "Malawi", city: "Feldkirchen in Kärnten"},
-//   {id: "6", name: "Mason Porter", salary: "$78,615", country: "Chile", city: "Gloucester"},
-// ];
-
 class Devices extends Component {
   getDevices(){
     let { error, loading, devices } = this.props;
@@ -41,12 +32,13 @@ class Devices extends Component {
       { Header: 'Action',
         Cell: (row)=>(
           <div className="action-right">
-            <Link to={`/devices/${row.row.id}`} className="btn-simple btn-icon btn btn-warning">
+            <Link to={`/devices/${row.original.id}`} className="btn-simple btn-icon btn btn-warning">
               <i className="fa fa-edit"></i>
             </Link>
           </div>
         ),
-        filterable: false, maxWidth: 100
+        filterable: false,
+        maxWidth: 100
       },
     ];
 
