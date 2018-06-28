b999393ff0f4baf5bdfd2ad2e3c9350e368ec976
OLIVIER MASUREL <olivier.masurel@decathlon.COM>
2018-06-28 11:18:01 +0200

    Update seeders for First name

--- seeders/20180620081520-demo-users.js
+++ seeders/20180620081520-demo-users.js
@@ -4,8 +4,8 @@ module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.bulkInsert('users', [
       {id: "40dc09ad-c035-47cf-8587-8622c4319d86",
-      first_name: "jeanphilippe",
-      last_name: "bornier",
+      first_name: "Jeanphilippe",
+      last_name: "Bornier",
       email: "jeanphilippe.bornier@decathlon.com",
       p2pe_agreement: 1,
       language: "en",
@@ -15,8 +15,8 @@ module.exports = {
       updatedAt: new Date()
       },
       {id: "700cb59b-91fd-42b3-aa41-3702f0b8611d",
-      first_name:"yuanqin",
-      last_name: "deng",
+      first_name:"Yuanqin",
+      last_name: "Deng",
       email: "yuanqin.deng@decathlon.com",
       p2pe_agreement: 1,
       language: "en",
@@ -26,8 +26,8 @@ module.exports = {
       updatedAt: new Date()
       },
       {id:"ae6233bb-90db-42bc-8f18-17a0d31c0100",
-      first_name:"olivier",
-      last_name: "masurel",
+      first_name:"Olivier",
+      last_name: "Masurel",
       email: "olivier.masurel@decathlon.com",
       p2pe_agreement: 0,
       language: "en",
@@ -37,8 +37,8 @@ module.exports = {
       updatedAt: new Date()
       },
       {id:"1d72faa0-318a-44c1-a15a-87f583094d7f",
-      first_name:"frederic",
-      last_name: "delimeux",
+      first_name:"Frederic",
+      last_name: "Delimeux",
       email: "frederic.delimeux@decathlon.com",
       p2pe_agreement: 0,
       language: "en",
